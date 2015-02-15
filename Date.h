9c620545c6aebd458c4f517a5c6585d58c372654
thulfeqar <anaqvi@live.com>
2015-02-15 15:25:16 -0500

    Update Date.h
    added some private variables into the header

--- Date.h
+++ Date.h
@@ -2,20 +2,30 @@
 #define DATE
 
 #include <ctime>
+#include <stdexcept>
 using namespace std;
 
 namespace SE
 {	
 	class Date
 	{
-		Date(unsigned char month, unsigned char day, unsigned year);
+		private:
+		unsigned char m;
+		unsigned char d;
+		int y;
+		struct tm sD;
+		time_t tD;
+		
+		public: 
+		//set date methods
+		Date(unsigned char month, unsigned char day, int year);
 		Date(tm date); //tm is a structure in ctime
 		Date(time_t epochTime); //time_t is the number of seconds since 1/1/1970
 		
+		//get date methods
 		unsigned char getMonth();
 		unsigned char getDay();
-		unsigned char getYear();
-		
+		int getYear();
 		struct tm getDate();
 		time_t getTime();
 	};
